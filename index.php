c62a04a056f187da9f3af7642ef4b33c60068e29
ioexcept <ioexcept@yahoo.com>
2014-10-13 19:07:45 -0500

    Initializatino

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Server Setup</title>
+</head>
+<body>
+Yes, yes, yes ... I work when I'm not busy<br>
+<?php print date("m/d/Y"); ?>
+<br>
+I am version 1.0
+</body>
+</html>
