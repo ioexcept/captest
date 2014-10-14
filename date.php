48232125ede72ffd1dfac7d4c4237e1d0eed3df7
ioexcept <ioexcept@yahoo.com>
2014-10-13 20:04:44 -0500

    updated

---
+++ date.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Capistrano Testing (Date)</title>
+</head>
+<body>
+Yes, yes, yes ... I work when I'm not busy * Date<br>
+<?php print date("m/d/Y"); ?>
+<br>
+I am version 2.1
+</body>
+</html>
